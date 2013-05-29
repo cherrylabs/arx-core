f9247c0f3419defa319ce7bc606f77c51b66713c
Daniel Sum <daniel@lacerisenumerique.be>
2013-05-29 12:17:39 +0200

    add unitTest

---
+++ tests/Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: danielsum
+ * Date: 29/05/13
+ * Time: 12:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Test extends PHPUnit_Framework_TestCase {
+
+}
