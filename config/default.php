f30bb1cca0043186976a475cd9da9394d60db5fa
Daniel Sum <daniel@lacerisenumerique.be>
2013-06-18 01:42:46 +0200

    no message

--- config/default.php
+++ config/default.php
@@ -8,11 +8,11 @@ $config = array();
 if(is_file(ROOT_DIR.DS.'arxConfig.php')){
     $config = include_once ROOT_DIR.DS.'arxConfig.php';
 } else {
-    $config['system']['app'] = 'Arx\c_app';
-    $config['system']['route'] = 'Arx\c_route';
-    $config['system']['template'] = 'Arx\c_template';
-    $config['system']['auth'] = 'Arx\c_auth';
-    $config['system']['db'] = 'Arx\a_db';
+    $config['system']['app'] = '\Arx\c_app';
+    $config['system']['route'] = '\Arx\c_route';
+    $config['system']['template'] = '\Arx\c_template';
+    $config['system']['auth'] = '\Arx\c_auth';
+    $config['system']['db'] = '\Arx\a_db';
 }
 
 return $config;
