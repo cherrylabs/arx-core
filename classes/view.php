1627d14225c6ca49502e7ef228d06940be70d9e9
Daniel Sum <daniel@lacerisenumerique.be>
2013-06-18 16:11:32 +0200

    no message

--- classes/view.php
+++ classes/view.php
@@ -1,25 +1,53 @@
 <?php namespace Arx;
 
-class c_view extends c_singleton {
+class c_view extends \Slim\View
+{
+    /**
+     * @var string The path to the directory containing Savant3.php and the Savant3 folder without trailing slash.
+     */
+    public static $savantDirectory = null;
 
-    private static $_aInstances = array();
+    /**
+     * @var array The options for the Savant3 environment, see http://phpsavant.com/api/Savant3/
+     */
+    public static $savantOptions = array('template_path' => 'views');
 
-    private static $_motor = 'c_template';
+    /**
+     * @var persistent instance of the Savant object
+     */
+    private static $savantInstance = null;
 
-    public function __construct(){
-
-    }
-
-    public static function fetch($template)
+    /**
+     * Renders a template using Savant3.php.
+     *
+     * @see View::render()
+     * @param string $template The template name specified in Slim::render()
+     * @return string
+     */
+    public function render($template)
     {
-        $t = new c_template();
-        return $t->fetch($template);
+        $savant = $this->getInstance();
+        $savant->assign($this->data);
+
+        return $savant->fetch($template);
     }
 
-    public static function display($template)
+    /**
+     * Creates new Savant instance if it doesn't already exist, and returns it.
+     *
+     * @throws RuntimeException If Savant3 lib directory does not exist.
+     * @return SavantInstance
+     */
+    private function getInstance()
     {
-        $t = new c_template();
-        return $t->display($template);
-    }
+        if (!self::$savantInstance) {
+            if (!is_dir(self::$savantDirectory)) {
+                throw new \RuntimeException('Cannot set the Savant lib directory : ' . self::$savantDirectory . '. Directory does not exist.');
+            }
 
+            self::$savantInstance = new c_template(self::$savantOptions);
+        }
+
+        return self::$savantInstance;
+    }
 }
