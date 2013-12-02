debd423ab1fb949ba6cf16d23f296a9d2e277c5b
Daniel Sum <daniel@lacerisenumerique.be>
2013-12-02 20:59:17 +0100

    Cleaning ARX and resolve paths conflict when using php artisan:serve

--- src/Arx/controllers/Base.php
+++ src/Arx/controllers/Base.php
@@ -9,20 +9,12 @@
 namespace Arx\controllers;
 
 use Controller;
-use View;
 /*
- * Betterbase controller and usefull function like setContent method
+ * Add some basic and usefull function like setContent method
  */
 
 class Base extends Controller {
-
-    protected $layout;
-
-    public function setContent($layout, $data = array()){
-        return $this->layout->content = View::make($layout, $data);
-    }
-
-    public function viewContent($layout, $data = array()){
-        return $this->layout->content = View::make($layout, $data);
+    public function setContent(){
+        
     }
 } 
