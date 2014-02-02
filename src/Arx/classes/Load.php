682865c27dc502f9ca84bf62225e06de8009cace
Daniel Sum <daniel@lacerisenumerique.be>
2014-02-02 21:43:57 +0100

    # add Load css, img, script helper class

--- src/Arx/classes/Load.php
+++ src/Arx/classes/Load.php
@@ -1,6 +1,6 @@
 <?php namespace Arx\classes;
 
-use Arx\classes\Utils as u;
+use Arx\classes\Html;
 
 /**
  * Class Load
@@ -12,190 +12,52 @@ use Arx\classes\Utils as u;
  */
 class Load {
 
-    private $sJs, $root, $key, $add, $sCSS;
-
-    public static function loadAll()
+    public static function js(array $array, $param = array())
     {
-        echo self::loadCSS();
-        echo self::loadJS();
-    } // loadAll
 
-    /**
-     * Load PHP files
-     * @author Daniel Sum
-     */
-    public static function loadPHP($sFiles = 'all', $mContext = array())
-    {
-        $mContext = u::toArray($mContext);
+        $out = "\n";
 
-        $root = '';
-
-        switch (true) {
-            case (is_array($sFiles)):
-                $sFiles = $sFiles;
-                break;
-
-            case (is_file($sFiles)):
-                $sFiles = array($sFiles);
-                break;
-
-            case (is_dir($sFiles)):
-                $sFiles = glob($sFiles.'/*.php');
-                break;
-
-            default:
-                predie($sFiles);
-                break;
-        }
-
-        if (!array($sFiles)) {
-            //if a context root is set
-            if (isset($mContext['root']) && $mContext['root'] == '-') {
-                $root = '';
-            } elseif (!empty($mContext['root'])) {
-                $root = $mContext['root'];
-                //else load from default INC repository
+        foreach($array as $key => $item){
+            if(is_array($item)){
+                $out .= Html::script($item[0], $item[1])."\n";
             } else {
-                $root = DIR_INC;
-            }
-
-            if (!is_array($sFiles)) {
-                $sFiles = explode(',', str_replace(' ','', $sFiles));
+                $out .= Html::script($item)."\n";
             }
         }
 
-        if(is_array($sFiles))
-        {
-            foreach ($sFiles as $sFp) {
-                $sFp = $root . $sFp;
 
-                if (!file_exists($sFp) OR is_dir($sFp)) {
-                    throw new Exception('The specified file ' . $sFp . ' cannot be found!');
-                } else {
-                    require_once $sFp;
-                }
-            }
+        return $out;
+    }
 
-            return true;
-        }
-
-        return false;
-    } // loadPHP
-
-    public static function PHP($sFiles = 'all' , $mContect = array())
+    public static function css(array $array , $param = array())
     {
-        self::loadPHP($sFiles, $mContect);
-    } // PHP (alias of loadPHP)
 
-    /**
-     * Load JS scripts
-     * @author Daniel Sum
-     */
-    public static function loadJS($sFiles = 'all' , $mContext = array())
-    {
-        $mContext = u::toArray($mContext);
+        $out = "\n";
 
-        if ($sFiles == 'all') {
-            if (!empty($mContext['root'])) {
-                $sFiles = c_fm::findIn($mContext['root'], array('dir' => JS));
+        foreach($array as $key => $item){
+            if(is_array($item)){
+                $out .= Html::style($item[0], $item[1])."\n";
             } else {
-                $appJs = c_fm::findIn(DIR_APPS, array('pattern' => '*/js/*.js'));
-
-                $sFiles = c_fm::findIn('', array('dir' => JS));
-
-                if (count($appJs) > 0) {
-                    // ...
-                }
+                $out .= Html::style($item)."\n";
             }
         }
 
-        if (!is_array($sFiles)) {
-            $sFiles = explode(',', str_replace(' ','', $sFiles));
-        }
+        return $out;
+    }
 
-        foreach ($sFiles as $sFp) {
-            $sFp = u::getUrlFile($sFp);
-
-            if (!strpos($sFp, '.only.') && !strpos($sFp, '.not.') && strpos($sFp, '.js')) {
-                $sJs .= '<script type="text/javascript" src="' . $sFp . '"></script>' . "\r\n";
-                c_hook::js($sCSS);
-            }
-        }
-
-        if (!empty($mContext['cat'])) {
-            foreach ($sFiles as $sFp) {
-                if (strpos($sFp, $mContext['cat'])) {
-                    $sJs .= '<script type="text/javascript" src="' . $sFp . '"></script>' . "\r\n";
-                }
-            }
-        }
-
-        return $sJs;
-    } // loadJS
-
-    public static function js($sFiles = 'all' , $mContext = array())
-    {
-        return self::loadJS($sFiles, $mContext);
-    } // js (alias of loadJS)
-
-    /**
-     * Load CSS scripts
-     * @author Daniel Sum
-     */
-    public static function loadCSS($sFiles = 'all' , $mContext = array())
+    public static function image(array $array , $param = array())
     {
 
+        $out = "\n";
 
-    } // loadCSS
-
-    public static function CSS($sFiles = 'all' , $mContext = array())
-    {
-        $sCSS = null;
-
-        $mContext = u::toArray($mContext);
-
-        if ($sFiles == 'all') {
-            if (!empty($mContext['root'])) {
-                $sFiles = c_fm::findIn($mContext['root'], array('dir' => CSS));
+        foreach($array as $key => $item){
+            if(is_array($item)){
+                $out .= Html::image($item[0], $item[1])."\n";
             } else {
-                $sFiles = $GLOBALS['allCSS'];
+                $out .= Html::image($item)."\n";
             }
         }
 
-        //if a context root is set
-        if (!empty($mContext['root'])) {
-            $root = $mContext['root'];
-        } else {
-            //else load from default CSS repository
-            $root = asset('css');
-        }
-
-        if (!is_array($sFiles)) {
-            $sFiles = explode(',', str_replace(' ','', $sFiles));
-        }
-
-        //preventdefault exclude
-        $mContext['exclude'][] = '.not.';
-
-        foreach ($sFiles as $key=>$sFp) {
-            $sFp = u::getUrlFile($sFp);
-
-            if (!in_array(str_replace(CSS.DS,'',$sFp), $mContext['exclude'])) {
-
-                if ( !isset($mContext['add']) ) {
-                    $mContext['add'] = '';
-                }
-
-                if (strpos($sFp, '.print.')) {
-                    $sCSS .= '<link rel="stylesheet" media="print" href="'. $sFp .'" type="text/css" '. (!empty($mContext['add'][$key]) ? $mContext['add'][$key] : $mContext['add']) .' />  '."\r\n";
-                } elseif (strpos($sFp, '.css')) {
-                    $sCSS .= '<link rel="stylesheet"  href="'. $sFp .'" type="text/css" '. (!empty($mContext['add'][$key]) ? $mContext['add'][$key] : $mContext['add']) .' />'."\r\n";
-                }
-                //c_hook::css($sCSS);
-
-            }
-        }
-
-        return $sCSS;
-    } // CSS (alias of loadCSS)
+        return $out;
+    }
 }
