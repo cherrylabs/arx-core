fce26c209209bd52d8fd7fbb319c009fcd3c9599
Daniel Sum <daniel@cherrypulp.com>
2014-04-03 13:24:53 +0200

    - Add new Arr::overwrite method - add Lang facade that enable Lang::merge - add default class template

---
+++ src/views/templates/class.tpl.php
@@ -0,0 +1,42 @@
+<?=$namespace?>;
+
+/**
+ * <?=$classname?>
+ */
+
+class <?=$classname?>
+{
+
+# Protected Methods
+
+#A
+
+#B
+
+#C
+
+#D
+
+#E
+
+#F
+
+#G
+
+#H
+
+#I
+
+#J
+
+#K
+
+#L
+
+#P
+
+#R
+
+#S
+
+} // <?=$classname?>
