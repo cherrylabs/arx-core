7026a88c15f2a70de532b9905a1a082603a114f3
Daniel Sum <daniel@cherrypulp.com>
2014-08-01 12:28:21 +0200

    - add Assets old class compatibility

---
+++ src/Arx/classes/Assets.php
@@ -0,0 +1,13 @@
+<?php namespace Arx\classes;
+
+use Arx;
+
+/**
+ * Class Assets
+ *
+ * @deprecated since 1.4.1 please use Asset instead !
+ * @package Arx\classes
+ */
+class Assets extends Asset {
+    
+} 
