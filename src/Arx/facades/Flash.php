0edcfedded1a37ea3039a10cfa35a16d64cf4971
Daniel Sum <daniel@lacerisenumerique.be>
2015-12-29 16:28:10 +0100

    - Add a notifier helper

---
+++ src/Arx/facades/Flash.php
@@ -0,0 +1,31 @@
+<?php namespace Arx\facades;
+
+/**
+ * Class View
+ *
+ * Override default View facade
+ *
+ * @package Arx\facades
+ */
+class Flash extends \Arx\classes\Facade {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'arx.flash'; }
+
+    /**
+     * Get a evaluated view contents for the given view.
+     *
+     * @param  string  $view
+     * @param  array   $data
+     * @param  array   $mergeData
+     * @return \Illuminate\View\View
+     */
+    public static function make($view, $data = array(), $mergeData = array())
+    {
+       return self::resolve($view, $data, $mergeData);
+    }
+
+}
