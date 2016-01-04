bb79727d2b35df71775678dc681a4d89b9bfc6ac
Daniel Sum <daniel@lacerisenumerique.be>
2016-01-04 11:58:11 +0100

    - add a OnlyNotEmpty request on array

--- src/Arx/classes/Request.php
+++ src/Arx/classes/Request.php
@@ -10,8 +10,13 @@ use Arx;
  * @package Arx\classes
  */
 class Request {
+
 	public static function __callStatic($name, $args){
 		$app = Arx::getInstance()['request'];
 		return call_user_func_array(array($app, $name), $args);
+	}
+
+	public function onlyNotEmpty($array){
+		return $this->only(array_filter($array));
 	}
 }
