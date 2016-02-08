11f403bed088fc91301518a211e63f0e7992f60c
Daniel Sum <daniel@lacerisenumerique.be>
2016-02-08 16:48:38 +0100

    Update html.tpl.php
    - fix error in title

--- src/views/html.tpl.php
+++ src/views/html.tpl.php
@@ -13,7 +13,7 @@ $lang = Lang::getLocale() ?: 'en';
 <head>
     @section('head')
         <meta charset="UTF-8">
-        <title><?php echo $this->headtitle ?></title>
+        <title>@yield('head_title')</title>
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
